c299b81e98e1f9e85c0f318bba1a796aea5fdcfc
KamranNaveed <kamran.naveedsyed@gmail.com>
2023-01-02 13:21:32 -0500

    prepared for production

--- api/index.js
+++ api/index.js
@@ -54,3 +54,14 @@ app.use("/api/posts", postRoute)
 app.listen(process.env.PORT, () => {
     console.log("Backend server running")
 })
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
